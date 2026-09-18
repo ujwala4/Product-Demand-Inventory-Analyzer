03003f9c29330823ac58632d0487ed43721c5d50
ujwala4 <ujwala@ictsystemsindia.com>
2026-09-18 20:20:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -407,19 +407,13 @@ with gr.Blocks(
             outputs=search_results
         )
 
-
 if __name__ == "__main__":
     import os
 
-    port = int(
-        os.environ.get(
-            "PORT",
-            7860
-        )
-    )
+    port = int(os.environ.get("PORT", 7860))
 
     app.launch(
         server_name="0.0.0.0",
         server_port=port
     )
-
+    
